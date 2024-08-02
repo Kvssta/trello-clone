2b28badb9459ac3f0d9917336144045f446e9127
Petar <nikolakostadinovic735@gmail.com>
2024-08-03 01:48:37 +0200

    first commit

---
+++ src/main/java/org/nikola/trelloclone/TrelloCloneApplication.java
@@ -0,0 +1,13 @@
+package org.nikola.trelloclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloCloneApplication.class, args);
+    }
+
+}
